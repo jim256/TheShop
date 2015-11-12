3a188bfe30db540fc6d1736bfcebe50f1c24806e
jim256 <jared8c@gmail.com>
2015-11-12 13:48:37 -0700

    Made database string, updated RepairStatus and Admin

--- TheShop/Admin/Admin.aspx.designer.cs
+++ TheShop/Admin/Admin.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace TheShop.Admin {
         protected global::System.Web.UI.WebControls.GridView gvCurrentRepairs;
         
         /// <summary>
+        /// sqlCurrentRepairs control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource sqlCurrentRepairs;
+        
+        /// <summary>
         /// lblPastRepairs control.
         /// </summary>
         /// <remarks>
